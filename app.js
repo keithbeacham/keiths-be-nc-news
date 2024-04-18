608d318b26d13ac4884e8a85cc0187cc78115259
Keith <keith@beachams.net>
2024-04-18 12:35:57 +0100

    updated app.js to use express routers

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
 const express = require("express");
-const {
-  healthCheck,
-  getAllTopics,
-  getEndpoints,
-  getArticleById,
-  getArticles,
-  getCommentsByArticleId,
-  postCommentByArticleId,
-  patchArticleById,
-} = require("./controllers/articles.controllers");
+const apiRouter = require("./routes/api-router");
 const {
   customErrorHandler,
   psqlErrorHandler,
   displayError,
 } = require("./errors/index");
-const { deleteCommentById } = require("./controllers/comments.controllers");
-const { getAllUsers } = require("./controllers/users.controllers");
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/api/healthcheck", healthCheck);
-app.get("/api/topics", getAllTopics);
-app.get("/api", getEndpoints);
-app.get("/api/articles/:article_id", getArticleById);
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-app.post("/api/articles/:article_id/comments", postCommentByArticleId);
-app.patch("/api/articles/:article_id", patchArticleById);
-app.delete("/api/comments/:comment_id", deleteCommentById);
-app.get("/api/users", getAllUsers);
-
+app.use("/api", apiRouter);
 app.use(customErrorHandler);
 app.use(psqlErrorHandler);
 
