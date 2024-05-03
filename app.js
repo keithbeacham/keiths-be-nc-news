e94b2bfb41e49d6fe7d7c5785a618ca5c530a629
Keith <keith@beachams.net>
2024-05-03 16:00:26 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const {
   psqlErrorHandler,
   displayError,
 } = require("./errors/index");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(customErrorHandler);
